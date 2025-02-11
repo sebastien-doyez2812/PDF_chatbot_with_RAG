12c50258b255c7f4b1ea82e434f897b29a21d1a9
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-02-10 22:17:02 -0500

    add the main file

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Author: Sébastien Doyez
+date: 10/02/2025
+"""
+
+import os
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
+
+
+
+
+
