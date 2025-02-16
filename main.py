449b9864419a72e658ef1a7219f8c21374051d82
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-02-15 22:02:03 -0500

    add the Gemma lmm and update the names of the python files

--- main.py
+++ main.py
@@ -1,112 +1,52 @@
-"""
-Author: Sébastien Doyez
-date: 10/02/2025
-"""
 import argparse
-import os
-import shutil
-from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.schema.document import Document
-from langchain_community.embeddings.ollama import OllamaEmbeddings
+from embedding import get_embedding_function
 from langchain.vectorstores.chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
 
 
+CHROMA = "chroma"
+NB_CONTEXTS = 5
 
-DATA_PATH = "C:/Users/doyez/Documents/PDF_chatbot_with_RAG/docs/"
-CHROMAPATH = "chroma"
+PROMPT = """
+Answer the question based only on this following context:
+{context}
 
+Answer the question based on the above context:{question}
 
-
-def load_documents():
-    document_loader = PyPDFDirectoryLoader(DATA_PATH)
-    return document_loader.load()
-
-
-def get_embedding_function():
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
-    return embeddings
+"""
 
 
-
-def split_documents(documents: list[Document]):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 800,
-        chunk_overlap = 80,
-        length_function=len,
-        is_separator_regex = False
-    )
-    return text_splitter.split_documents(documents)
-
-def add_to_chroma(chunks:list[Document]):
-    db = Chroma(persist_directory=CHROMAPATH,
-                embedding_function= get_embedding_function())
-    
-
-    chunks_with_ids = calculate_id_chunks(chunks)
-
-    existing_items = db.get(include = [])
-    # Create a set: a set contains only uniques values ( no duplicates data!)
-    existing_ids = set(existing_items["ids"])
-    print(f" There are {len(existing_ids)} existings docs!")
-
-    # Add only the new chunks of data in db
-    new_chunks = []
-    for chunk in chunks_with_ids:
-        if chunk.metadata["id"] not in existing_ids:
-            new_chunks.append(chunk)
-    
-    if len(new_chunks):
-        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
-        db.add_documents(new_chunks, ids= new_chunk_ids)
-        db.persist()
-        print(f"[+] Adding Documennts done!")
-
-    else:
-        print("[i] No new documents...")
-
-def calculate_id_chunks(chunks):
-    page_id = 0
-    last_page_id = None
-    current_chunk_index = 0
-    
-    for chunk in chunks:
-        source = chunk.metadata.get("source")
-        page = chunk.metadata.get("page")
-
-        current_page_id = f"{source}:{page}"
-        if current_page_id == last_page_id:
-            current_chunk_index +=1
-
-        else:
-            current_chunk_index = 0
-
-        chunk_id = f"{current_page_id}:{current_chunk_index}"
-        last_page_id = current_page_id
-
-        chunk.metadata["id"] = chunk_id
-    return chunks
-
-
-def clear_db():
-    if os.path.exists(CHROMAPATH):
-        shutil.rmtree(CHROMAPATH)
 
 def main():
 
+    # To get the query text (from the argument arg1 (python main.py arg1))
     parser = argparse.ArgumentParser()
-    # action = store_true mean if the --reset option is here, 
-    # put reset to true...
-    parser.add_argument("--reset", action="store_true", help= " Reset the Vector Database" )
+    parser.add_argument("query_text", type=str, help= "This is the query text")
     args = parser.parse_args()
-    if args.reset:
-        print("[i] Clear the database...")
-        clear_db()
 
-    documents = load_documents()
-    chunks = split_documents(documents)
-    add_to_chroma(chunks)
+    query_text = args.query_text
+    query(query_text)
 
+
+
+def query(query_text:str):
+    embd = get_embedding_function()
+    db =  Chroma(persist_directory = CHROMA, embedding_function= embd)
+
+    results = db.similarity_search_with_score(query_text, k=NB_CONTEXTS)
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+
+    prompt_template = ChatPromptTemplate.from_template(PROMPT)
+    prompt = prompt_template.format(context=context_text, question= query_text)
+
+    model = Ollama(model="gemma")
+    rep = model.invoke(prompt)
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    formatted_response = f"Response: {rep}\nSOurces:{sources}"
+    print(formatted_response)
+    return rep
 
 if __name__ == "__main__":
     main()
